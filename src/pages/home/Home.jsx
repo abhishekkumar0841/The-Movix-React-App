3e5790782f7b791260bd3c562803aced89ad87a8
abhishekkumar <abhishekkumar0841@gmail.com>
2023-11-14 22:12:34 +0530

    TopRated.jsx added & HomePage completed here

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import './home.scss'
-import HeroBanner from './heroBanner/HeroBanner'
-import Trending from './trending/Trending'
-import Popular from './popular/Popular'
+import React from "react";
+import "./home.scss";
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
-    <div className='homePage'>
-      <HeroBanner/>
-      <Trending/>
-      <Popular/>
-      <div style={{height: '1000px'}}></div>
+    <div className="homePage">
+      <HeroBanner />
+      <Trending />
+      <Popular />
+      <TopRated />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
