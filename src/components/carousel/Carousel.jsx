1c6cd34abcb68bf28306dd6dca9ef0d30d31d5b8
abhishekkumar <abhishekkumar0841@gmail.com>
2023-11-14 22:04:55 +0530

    Popular.jsx added

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -15,7 +15,7 @@ import "./carousel.scss";
 import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
 
-const Carousel = ({ data, loading }) => {
+const Carousel = ({ data, loading, endpoint }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
@@ -66,7 +66,7 @@ const Carousel = ({ data, loading }) => {
               return (
                 <div
                   key={item?.id}
-                  onClick={() => navigate(`/${item.media_type}/${item.id}`)}
+                  onClick={() => navigate(`/${item.media_type || endpoint}/${item.id}`)}
                   className="carouselItem"
                 >
                   <div className="posterBlock">
