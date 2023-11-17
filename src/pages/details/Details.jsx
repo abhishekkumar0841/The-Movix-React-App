5a03550772f8efc35a346bc834a669510ae7d8fc
abhishekkumar <abhishekkumar0841@gmail.com>
2023-11-17 20:59:47 +0530

    VideoSection.jsx added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,7 @@ import useFetch from '../../hooks/useFetch'
 import { useParams } from 'react-router-dom'
 import DetailsBanner from './detailsBanner/DetailsBanner'
 import Cast from './cast/Cast'
+import VideosSection from './videoSection/VideoSection'
 
 const Details = () => {
 
@@ -16,6 +17,7 @@ const Details = () => {
     <div>
       <DetailsBanner video={data?.results[0]} crew={credits?.crew}/>
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading}/>
     </div>
   )
 }
